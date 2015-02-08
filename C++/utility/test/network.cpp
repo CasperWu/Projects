1776f02958413c60885186197e8aa8824a1bc23d
Casper Wu <casper.wu@hotmail.com>
2015-02-08 18:46:48 +0800

    Perform UT by gtest

--- C++/utility/test/network.cpp
+++ C++/utility/test/network.cpp
@@ -2,21 +2,28 @@
 #include <cassert>
 #include <iostream>
 
+#include <gtest/gtest.h>
+
 #include "network.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
+{
+    cout << "Running main() from gtest_main.cc\n";  
+    testing::InitGoogleTest(&argc, argv);  
+    return RUN_ALL_TESTS();  
+}
+
+
+TEST(ChecksumIp, RandomCheck)
 {
     /* Here a IP header captured by tcpdumpa.
      */
     const unsigned char ip[] = {0x45, 0x00, 0x00, 0x28, 0xef, 0xee, 0x40, 0x00, 0x40, 0x06, 0x37, 0x3e, 0xc0, 0xa8, 0x96, 0x83, 0xc0, 0x1e, 0xfc, 0x58};
     const sniff_ip *ipHeader = (const sniff_ip *)ip;
 
-    unsigned short csum = ChecksumIp(ip, sizeof(ip));
-    if (ipHeader->ip_sum != csum) {
-        cout << "Checksum validation failed\n";
-    } else {
-        cout << "Checksum validation successful\n";
-    }
+    const unsigned short csum = ChecksumIp(ip, sizeof(ip));
+
+    EXPECT_EQ(ipHeader->ip_sum, csum);
 }
